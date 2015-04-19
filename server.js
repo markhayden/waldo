99a6034d9075c10e46d66a768be88c91e9c5adac
Mark Hayden <hi@markhayden.me>
2015-04-19 01:23:34 -0700

    hrm

--- server.js
+++ server.js
@@ -1,19 +1,56 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-// var sherlock = require('sherlock-inspector');
-// var segment = require('sherlock-segment');
+'use strict';
 
-var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+/*
+var cl = console.log;
+console.log = function(){
+  console.trace();
+  cl.apply(console,arguments);
+};
+*/
+
+// Requires meanio .
+var mean = require('meanio');
+var cluster = require('cluster');
 
 
-app.get('/test', function(req, res) {
-  res.json({
-    url: "sup dawg",
-    data: out
-  });
+// Code to run if we're in the master process or if we are not in debug mode/ running tests
+
+if ((cluster.isMaster) &&
+  (process.execArgv.indexOf('--debug') < 0) &&
+  (process.env.NODE_ENV!=='test') && (process.env.NODE_ENV!=='development') &&
+  (process.execArgv.indexOf('--singleProcess')<0)) {
+//if (cluster.isMaster) {
+
+    console.log('for real!');
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
+
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        console.log ('forking ',i);
+        cluster.fork();
+    }
+
+    // Listen for dying workers
+    cluster.on('exit', function (worker) {
+        // Replace the dead worker, we're not sentimental
+        console.log('Worker ' + worker.id + ' died :(');
+        cluster.fork();
+
+    });
+
+// Code to run if we're in a worker process
+} else {
+
+    var workerId = 0;
+    if (!cluster.isMaster)
+    {
+        workerId = cluster.worker.id;
+    }
+// Creates and serves mean application
+    mean.serve({ workerid: workerId /* more options placeholder*/ }, function (app) {
+      var config = app.config.clean;
+        var port = config.https && config.https.port ? config.https.port : config.http.port;
+        console.log('Mean app started on port ' + port + ' (' + process.env.NODE_ENV + ') cluster.worker.id:', workerId);
+    });
 }
-
-app.listen(8080, '0.0.0.0');
-console.log('Listening on port 8080');
